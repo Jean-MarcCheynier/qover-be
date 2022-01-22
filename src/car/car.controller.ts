7b0fe591f981302266daa2dc5f0131df19a88db9
Jean-MarcCheynier <jmcheynier@gmail.com>
2022-01-22 10:07:38 +0100

    rename to qover-be

--- src/car/car.controller.ts
+++ src/car/car.controller.ts
@@ -11,12 +11,15 @@ import { CarService } from './car.service';
 import { CreateCarDto } from './dto/create-car.dto';
 import { UpdateCarDto } from './dto/update-car.dto';
 import { ApiTags } from '@nestjs/swagger';
+import { Roles } from 'src/common/decorator/roles.decorator';
+import { Role } from 'src/common/enum/roles.enum';
 
 @ApiTags('Car')
 @Controller('car')
 export class CarController {
   constructor(private readonly carService: CarService) {}
 
+  @Roles(Role.ADMIN)
   @Post()
   create(@Body() createCarDto: CreateCarDto) {
     return this.carService.create(createCarDto);
@@ -32,11 +35,13 @@ export class CarController {
     return this.carService.findOne(+id);
   }
 
+  @Roles(Role.ADMIN)
   @Patch(':id')
   update(@Param('id') id: string, @Body() updateCarDto: UpdateCarDto) {
     return this.carService.update(+id, updateCarDto);
   }
 
+  @Roles(Role.ADMIN)
   @Delete(':id')
   remove(@Param('id') id: string) {
     return this.carService.remove(+id);
